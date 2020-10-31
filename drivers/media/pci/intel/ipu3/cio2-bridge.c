cff955ac89f23a2c92845570266e40fdb6fc047a
Daniel Scally <djrscally@gmail.com>
2020-10-31 00:11:42 +0000

    ipu3-cio2: Add functionality to parse power resources and peripheral GPIOs from ACPI and map them to sensors
    GPIOs assigned to INT3472 entries with a particular value in their CLDB buffer seem to represent regulators and GPIO lines that are intended to be consumed by sensor drivers. Map them.

--- drivers/media/pci/intel/ipu3/cio2-bridge.c
+++ drivers/media/pci/intel/ipu3/cio2-bridge.c
@@ -2,11 +2,15 @@
 // Author: Dan Scally <djrscally@gmail.com>
 #include <linux/acpi.h>
 #include <linux/device.h>
+#include <linux/dmi.h>
 #include <linux/i2c.h>
 #include <linux/kernel.h>
+#include <linux/list.h>
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/platform_device.h>
 #include <linux/property.h>
+#include <linux/regulator/driver.h>
 #include <media/v4l2-subdev.h>
 
 #include "cio2-bridge.h"
@@ -25,7 +29,23 @@ static struct software_node cio2_hid_node = { CIO2_HID };
 static struct cio2_bridge bridge;
 
 static const char * const port_names[] = {
-	"port0", "port1", "port2", "port3",
+	"port0", "port1", "port2", "port3"
+};
+
+static int cio2_bridge_gpio_regulator_is_enabled(struct regulator_dev *rdev)
+{
+	struct gpio_desc *gpio;
+	int value;
+
+	gpio = rdev_get_drvdata(rdev);
+
+	value = gpiod_get_value_cansleep(gpio);
+
+	return value;
+}
+
+static struct regulator_ops cio2_bridge_gpio_regulator_ops = {
+	.is_enabled         = cio2_bridge_gpio_regulator_is_enabled,
 };
 
 static const struct property_entry remote_endpoints[] = {
@@ -58,8 +78,10 @@ static int read_acpi_block(struct device *dev, char *id, void *data, u32 size)
 	handle = ACPI_HANDLE(dev);
 
 	status = acpi_evaluate_object(handle, id, NULL, &buffer);
-	if (ACPI_FAILURE(status))
+	if (ACPI_FAILURE(status)) {
+		pr_info("%s: %d\n", __func__, 2);
 		return -ENODEV;
+	}
 
 	obj = buffer.pointer;
 	if (!obj) {
@@ -105,6 +127,17 @@ static int get_acpi_ssdb_sensor_data(struct device *dev,
 	return 0;
 }
 
+static int get_acpi_cldb_pmic_data(struct device *dev, struct pmic_cldb *cldb)
+{
+	int ret;
+
+	ret = read_acpi_block(dev, "CLDB", cldb, sizeof(*cldb));
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
 static int create_fwnode_properties(struct sensor *sensor,
 				    struct sensor_bios_data *ssdb)
 {
@@ -114,14 +147,14 @@ static int create_fwnode_properties(struct sensor *sensor,
 	int i;
 
 	/* device fwnode properties */
-	memset(dev_properties, 0, sizeof(dev_properties));
+	memset(dev_properties, 0, sizeof(struct property_entry) * 3);
 
 	dev_properties[0] = PROPERTY_ENTRY_U32("clock-frequency",
 					       ssdb->mclkspeed);
 	dev_properties[1] = PROPERTY_ENTRY_U8("rotation", ssdb->degree);
 
 	/* endpoint fwnode properties */
-	memset(ep_properties, 0, sizeof(ep_properties));
+	memset(ep_properties, 0, sizeof(struct property_entry) * 4);
 
 	sensor->data_lanes = kmalloc_array(ssdb->lanes, sizeof(u32), GFP_KERNEL);
 	if (!sensor->data_lanes)
@@ -137,7 +170,7 @@ static int create_fwnode_properties(struct sensor *sensor,
 	ep_properties[2] = remote_endpoints[(bridge.n_sensors * 2) + ENDPOINT_SENSOR];
 
 	/* cio2 endpoint props */
-	memset(cio2_properties, 0, sizeof(cio2_properties));
+	memset(cio2_properties, 0, sizeof(struct property_entry) * 3);
 
 	cio2_properties[0] = PROPERTY_ENTRY_U32_ARRAY_LEN("data-lanes",
 							  sensor->data_lanes,
@@ -152,7 +185,7 @@ static int create_connection_swnodes(struct sensor *sensor,
 {
 	struct software_node *nodes = sensor->swnodes;
 
-	memset(nodes, 0, sizeof(nodes));
+	memset(nodes, 0, sizeof(*nodes) * 6);
 
 	nodes[SWNODE_SENSOR_HID] = NODE_SENSOR(sensor->name,
 					       sensor->dev_properties);
@@ -168,6 +201,302 @@ static int create_connection_swnodes(struct sensor *sensor,
 						    sensor->cio2_properties);
 
 	return 0;
+}
+
+static int cio2_bridge_find_sensor_pmic(struct sensor *sensor)
+{
+	struct acpi_handle_list dep_handles;
+	acpi_handle handle;
+	acpi_status status;
+	unsigned int i;
+	int ret;
+
+	handle = sensor->adev->handle;
+
+	if (!acpi_has_method(handle, "_DEP")) {
+		dev_err(sensor->dev, "Sensor has no _DEP method\n");
+		return -EINVAL;
+	}
+
+	status = acpi_evaluate_reference(handle, "_DEP", NULL, &dep_handles);
+	if (ACPI_FAILURE(status)) {
+		dev_err(sensor->dev, "Failed to evaluate _DEP reference\n");
+		return -EINVAL;
+	}
+
+	for (i = 0; i < dep_handles.count; i++) {
+		struct acpi_device_info *info;
+
+		status = acpi_get_object_info(dep_handles.handles[i], &info);
+		if (ACPI_FAILURE(status)) {
+			dev_err(sensor->dev,
+				"Failed to get _DEP device information\n");
+			return -EINVAL;
+		}
+
+		if (info->valid & ACPI_VALID_HID &&
+		    !strcmp(info->hardware_id.string, PMIC_HID)) {
+			ret = acpi_bus_get_device(dep_handles.handles[i],
+						  &sensor->pmic.adev);
+			if (ret) {
+				dev_err(sensor->dev,
+					"Failed to get PMIC ACPI device\n");
+				ret = -ENODEV;
+			}
+			kfree(info);
+			break;
+		}
+		kfree(info);
+	}
+
+	if (!sensor->pmic.adev)
+		return ret;
+
+	sensor->pmic.dev = bus_find_device_by_acpi_dev(&platform_bus_type,
+						   sensor->pmic.adev);
+
+	if (!sensor->pmic.dev) {
+		acpi_dev_put(sensor->pmic.adev);
+		return -ENODEV;
+	}
+
+	INIT_LIST_HEAD(&sensor->pmic.regulators);
+	INIT_LIST_HEAD(&sensor->pmic.gpios);
+
+	return 0;
+}
+
+static struct cio2_sensor_regulator_map *
+cio2_bridge_get_sensor_supply_map(struct sensor *sensor)
+{
+	struct cio2_sensor_regulator_map *ret;
+	union acpi_object *obj;
+	unsigned int i;
+
+	obj = acpi_evaluate_dsm_typed(sensor->adev->handle,
+				      &sensor_module_guid, 0x00,
+				      0x00, NULL, ACPI_TYPE_STRING);
+
+	if (!obj) {
+		dev_err(sensor->dev,
+			"Failed to get sensor module string from _DSM\n");
+		return ERR_PTR(-ENODEV);
+	}
+
+	if (obj->string.type != ACPI_TYPE_STRING) {
+		dev_err(sensor->dev, "Sensor _DSM returned a non-string value\n");
+		ret = ERR_PTR(-EINVAL);
+		goto out_free_obj;
+	}
+
+	ret = -ENODEV;
+	for (i = 0; i < ARRAY_SIZE(cio2_sensor_regulator_maps); i++) {
+		if (!strcmp(cio2_sensor_regulator_maps[i].sensor_module_name,
+			    obj->string.pointer)) {
+			ret = &cio2_sensor_regulator_maps[i];
+			goto out_free_obj;
+		}
+	};
+
+out_free_obj:
+	ACPI_FREE(obj);
+	return ret;
+}
+
+static int cio2_bridge_register_regulator(struct sensor *sensor,
+					  struct acpi_resource *ares)
+{
+	struct cio2_sensor_regulator_map *regulator_map;
+	struct regulator_init_data init_data = { };
+	struct cio2_gpio_regulator *regulator;
+	struct regulator_config cfg = { };
+	struct pmic *pmic = &sensor->pmic;
+	int ret;
+
+	regulator_map = cio2_bridge_get_sensor_supply_map(sensor);
+	if (IS_ERR_OR_NULL(regulator_map)) {
+		dev_err(sensor->dev,
+			"Found no supplies defined for this sensor\n");
+		return PTR_ERR(regulator_map);
+	}
+
+	if (pmic->n_regulators >= regulator_map->n_supplies) {
+		dev_err(sensor->dev, "All known supplies are already mapped\n");
+		return -EINVAL;
+	}
+
+	init_data.supply_regulator = NULL;
+	init_data.constraints.valid_ops_mask = REGULATOR_CHANGE_STATUS;
+	init_data.num_consumer_supplies = 1;
+	init_data.consumer_supplies = &regulator_map->supplies[pmic->n_regulators];
+
+	regulator = kmalloc(sizeof(*regulator), GFP_KERNEL);
+	if (!regulator)
+		return -ENOMEM;
+
+	snprintf(regulator->regulator_name, GPIO_REGULATOR_NAME_LENGTH,
+		 "gpio-regulator-%d", pmic->n_regulators);
+	snprintf(regulator->supply_name, GPIO_REGULATOR_SUPPLY_NAME_LENGTH,
+		 "supply-%d", pmic->n_regulators);
+
+	regulator->rdesc = GPIO_REGULATOR(regulator->regulator_name,
+					 regulator->supply_name,
+					 pmic->n_regulators,
+					 &cio2_bridge_gpio_regulator_ops);
+
+	regulator->gpio = acpi_get_gpiod(ares->data.gpio.resource_source.string_ptr,
+					 ares->data.gpio.pin_table[0]);
+	if (IS_ERR(regulator->gpio)) {
+		ret = PTR_ERR(regulator->gpio);
+		goto err_free_regulator;
+	}
+
+	cfg.dev = pmic->dev;
+	cfg.init_data = &init_data;
+	cfg.ena_gpiod = regulator->gpio;
+	cfg.driver_data = regulator->gpio;
+
+	regulator->rdev = regulator_register(&regulator->rdesc, &cfg);
+	if (IS_ERR(regulator->rdev)) {
+		ret = PTR_ERR(regulator->rdev);
+		goto err_free_gpio;
+	}
+
+	list_add(&regulator->list, &pmic->regulators);
+	pmic->n_regulators++;
+
+	return 0;
+
+err_free_gpio:
+	gpiod_put(regulator->gpio);
+err_free_regulator:
+	kfree(regulator);
+
+	return PTR_ERR(regulator->rdev);
+}
+
+static int cio2_bridge_map_gpio_to_sensor(struct sensor *sensor, struct acpi_resource *ares,
+					  char *func)
+{
+	struct pmic *pmic = &sensor->pmic;
+	struct gpiod_lookup table_entry;
+	struct cio2_gpio_pin *gpio;
+	struct acpi_device *adev;
+	acpi_handle handle;
+	acpi_status status;
+	int ret;
+
+	gpio = kzalloc(sizeof(*gpio) + sizeof(struct gpiod_lookup) * 2, GFP_KERNEL);
+	if (!gpio)
+		return -ENOMEM;
+
+	status = acpi_get_handle(NULL, ares->data.gpio.resource_source.string_ptr, &handle);
+	if (ACPI_FAILURE(status))
+		return -EINVAL;
+
+	ret = acpi_bus_get_device(handle, &adev);
+	if (ret)
+		return -ENODEV;
+
+	gpio->lookup->dev_id = dev_name(sensor->dev);
+	table_entry = (struct gpiod_lookup) GPIO_LOOKUP_IDX(acpi_dev_name(adev),
+							    ares->data.gpio.pin_table[0], func, 0,
+							    GPIO_ACTIVE_HIGH);
+	memcpy(&gpio->lookup->table[0], &table_entry, sizeof(table_entry));
+
+	gpiod_add_lookup_table(gpio->lookup);
+	list_add(&gpio->list, &pmic->gpios);
+
+	return 0;
+}
+
+static int cio2_bridge_map_pmic_gpio(struct acpi_resource *ares, void *data)
+{
+	struct sensor *sensor = data;
+	union acpi_object *obj;
+	int ret;
+
+	if ((ares->type != ACPI_RESOURCE_TYPE_GPIO) ||
+	   (ares->data.gpio.connection_type != ACPI_RESOURCE_GPIO_TYPE_IO))
+		return EINVAL; /* Deliberately positive */
+
+	/*
+	 * gpio_n + 2 because the index of this _DSM function is 1-based and the
+	 * first function is just a count.
+	 */
+	obj = acpi_evaluate_dsm_typed(sensor->pmic.adev->handle,
+				      &pmic_gpio_data_guid, 0x00,
+				      sensor->pmic.n_gpios + 2,
+				      NULL, ACPI_TYPE_INTEGER);
+	if (!obj) {
+		dev_warn(sensor->dev, "No _DSM entry for this GPIO pin\n");
+		return -ENODEV;
+	}
+
+	switch (obj->integer.value & 0xff) { /* low byte holds type information */
+	case 0x00: /* Maybe optional regulator? */
+		ret = cio2_bridge_map_gpio_to_sensor(sensor, ares, "xshutdn");
+		//cio2_bridge_register_regulator(sensor, gpio_n);
+		break;
+	case 0x01: /* Power regulators */
+	case 0x0c:
+		ret = cio2_bridge_register_regulator(sensor, ares);
+		break;
+	case 0x0b: /* Power rail, but to a device separate to sensor */
+		ret = cio2_bridge_register_regulator(sensor, ares);
+		break;
+	case 0x0d: /* Controls power to a privacy LED */
+		ret = cio2_bridge_map_gpio_to_sensor(sensor, ares, "indicator-led");
+		break;
+	default:
+		/* if we've gotten here, we're clueless */
+		dev_warn(sensor->pmic.dev,
+			"GPIO type 0x%02u not known; the sensor may not function\n",
+			(obj->integer.value & 0xff));
+		ret = -EINVAL;
+	}
+
+	sensor->pmic.n_gpios++;
+
+	ACPI_FREE(obj);
+	return ret;
+}
+
+static int cio2_bridge_parse_pmic_crs(struct sensor *sensor)
+{
+	struct pmic *pmic = &sensor->pmic;
+	struct list_head resource_list;
+	int ret = 0;
+
+	INIT_LIST_HEAD(&resource_list);
+
+	ret = acpi_dev_get_resources(pmic->adev, &resource_list,
+				     cio2_bridge_map_pmic_gpio, sensor);
+
+	acpi_dev_free_resource_list(&resource_list);
+
+	return ret;
+}
+
+static void cio2_bridge_free_gpio_resources(struct sensor *sensor)
+{
+	struct cio2_gpio_regulator *regulator;
+	struct pmic *pmic = &sensor->pmic;
+	struct cio2_gpio_pin *gpio;
+
+	list_for_each_entry(regulator, &pmic->regulators, list) {
+		regulator_unregister(regulator->rdev);
+		gpiod_put(regulator->gpio);
+		kfree(regulator);
+		pmic->n_regulators--;
+	}
+
+	list_for_each_entry(gpio, &pmic->gpios, list) {
+		gpiod_remove_lookup_table(gpio->lookup);
+		kfree(gpio->lookup);
+		kfree(gpio);
+		pmic->n_gpios--;
+	}
 }
 
 static void cio2_bridge_unregister_sensors(void)
@@ -192,6 +521,7 @@ static int connect_supported_devices(struct pci_dev *cio2)
 	struct sensor_bios_data ssdb;
 	struct fwnode_handle *fwnode;
 	struct acpi_device *adev;
+	struct pmic_cldb cldb;
 	struct sensor *sensor;
 	char * this_device;
 	struct device *dev;
@@ -222,17 +552,38 @@ static int connect_supported_devices(struct pci_dev *cio2)
 		if (ret)
 			goto err_free_dev;
 
-		ret = create_fwnode_properties(sensor, &ssdb);
+		ret = cio2_bridge_find_sensor_pmic(sensor);
 		if (ret)
 			goto err_free_dev;
+
+		ret = get_acpi_cldb_pmic_data(sensor->pmic.dev, &cldb);
+		if (ret)
+			goto err_free_pmic;
+
+		/*
+		 * If the INT3472 device upon which this sensor is dependent
+		 * has type "Discrete" then we have some setup work to do.
+		 */
+		if (cldb.control_logic_type == 1) {
+			pr_info("cio2: parsing _CRS\n");
+			ret = cio2_bridge_parse_pmic_crs(sensor);
+			if (ret)
+				goto err_free_pmic;
+		} else {
+			pr_info("cio2: control logic not equal to 1\n");
+		}
+
+		ret = create_fwnode_properties(sensor, &ssdb);
+		if (ret)
+			goto err_free_pmic;
 
 		ret = create_connection_swnodes(sensor, &ssdb);
 		if (ret)
-			goto err_free_dev;
+			goto err_free_pmic;
 
 		ret = software_node_register_nodes(sensor->swnodes);
 		if (ret)
-			goto err_free_dev;
+			goto err_free_pmic;
 
 		fwnode = software_node_fwnode(&sensor->swnodes[SWNODE_SENSOR_HID]);
 		if (!fwnode) {
@@ -253,6 +604,11 @@ static int connect_supported_devices(struct pci_dev *cio2)
 
 err_free_swnodes:
 	software_node_unregister_nodes(sensor->swnodes);
+err_free_gpio_resources:
+	cio2_bridge_free_gpio_resources(sensor);
+err_free_pmic:
+	acpi_dev_put(sensor->pmic.adev);
+	put_device(sensor->pmic.dev);
 err_free_dev:
 	put_device(dev);
 err_rollback:
@@ -319,15 +675,6 @@ err_put_cio2:
 
 void cio2_bridge_clean(struct pci_dev *cio2)
 {
-	/*
-	 * On some platforms, the cio2 device's fwnode is initially null until
-	 * set by cio2_bridge_init(). We need to reset it to null, otherwise
-	 * removing and inserting the module will cause issues.
-	 */
-
-	if (is_software_node(dev_fwnode(&cio2->dev)))
-		cio2->dev.fwnode = NULL;
-
 	pci_dev_put(cio2);
 
 	cio2_bridge_unregister_sensors();
--- drivers/media/pci/intel/ipu3/cio2-bridge.h
+++ drivers/media/pci/intel/ipu3/cio2-bridge.h
@@ -3,45 +3,106 @@
 #ifndef __CIO2_BRIDGE_H
 #define __CIO2_BRIDGE_H
 
+#include <linux/gpio/machine.h>
 #include <linux/property.h>
+#include <linux/regulator/machine.h>
 
-#define MAX_CONNECTED_DEVICES			4
-#define SWNODE_SENSOR_HID			0
-#define SWNODE_SENSOR_PORT			1
-#define SWNODE_SENSOR_ENDPOINT			2
-#define SWNODE_CIO2_PORT			3
-#define SWNODE_CIO2_ENDPOINT			4
-#define SWNODE_NULL_TERMINATOR			5
+#define MAX_CONNECTED_DEVICES				4
+#define SWNODE_SENSOR_HID				0
+#define SWNODE_SENSOR_PORT				1
+#define SWNODE_SENSOR_ENDPOINT				2
+#define SWNODE_CIO2_PORT				3
+#define SWNODE_CIO2_ENDPOINT				4
+#define SWNODE_NULL_TERMINATOR				5
 
-#define CIO2_HID				"INT343E"
-#define CIO2_PCI_ID				0x9d32
+#define CIO2_HID					"INT343E"
+#define CIO2_PCI_ID					0x9d32
+#define PMIC_HID					"INT3472"
 
-#define ENDPOINT_SENSOR				0
-#define ENDPOINT_CIO2				1
+#define ENDPOINT_SENSOR					0
+#define ENDPOINT_CIO2					1
 
-#define NODE_SENSOR(_HID, _PROPS)		\
-	((const struct software_node) {		\
-		.name = _HID,			\
-		.properties = _PROPS,		\
+#define CRS_END_TAG_DESCRIPTOR				0x79
+#define CRS_RESOURCE_SIZE_BITMASK			0x80
+#define CRS_RESOURCE_TYPE_BITMASK			0x7f
+#define CRS_RESOURCE_S_SIZE_BITMASK			0x07
+#define CRS_RESOURCE_L_SIZE_OFFSET_LOW			0x01
+#define CRS_RESOURCE_L_SIZE_OFFSET_HIGH			0x02
+#define CRS_GPIO_CONNECTION_TYPE_OFFSET			0x04
+#define CRS_GPIO_PIN_TABLE_OFFSET_LOW			0x0e
+#define CRS_GPIO_PIN_TABLE_OFFSET_HIGH			0x0f
+#define CRS_GPIO_RESOURCE_NAME_OFFSET_LOW		0x11
+#define CRS_GPIO_RESOURCE_NAME_OFFSET_HIGH		0x12
+#define CRS_GPIO_VENDOR_DATA_OFFSET_LOW			0x13
+#define CRS_GPIO_VENDOR_DATA_OFFSET_HIGH		0x14
+
+#define NODE_SENSOR(_HID, _PROPS)			\
+	((const struct software_node) {			\
+		.name = _HID,				\
+		.properties = _PROPS,			\
 	})
 
-#define NODE_PORT(_PORT, _SENSOR_NODE)		\
-	((const struct software_node) {		\
-		_PORT,				\
-		_SENSOR_NODE,			\
+#define NODE_PORT(_PORT, _SENSOR_NODE)			\
+	((const struct software_node) {			\
+		_PORT,					\
+		_SENSOR_NODE,				\
 	})
 
-#define NODE_ENDPOINT(_EP, _PORT, _PROPS)	\
-	((const struct software_node) {		\
-		_EP,				\
-		_PORT,				\
-		_PROPS,				\
+#define NODE_ENDPOINT(_EP, _PORT, _PROPS)		\
+	((const struct software_node) {			\
+		_EP,					\
+		_PORT,					\
+		_PROPS,					\
 	})
+
+#define GPIO_REGULATOR_NAME_LENGTH			17
+#define GPIO_REGULATOR_SUPPLY_NAME_LENGTH		9
+
+#define GPIO_REGULATOR(_NAME, _SUPPLY, _ID, _OPS)	\
+	((const struct regulator_desc) {		\
+		.name = _NAME,				\
+		.supply_name = _SUPPLY,			\
+		.id = _ID,				\
+		.type = REGULATOR_VOLTAGE,		\
+		.ops = _OPS,				\
+		.owner = THIS_MODULE,			\
+	})
+
+const guid_t pmic_gpio_data_guid = GUID_INIT(0x79234640, 0x9e10, 0x4fea, 0xa5,
+					     0xc1, 0xb5, 0xaa, 0x8b, 0x19, 0x75, 0x6f);
+
+const guid_t sensor_module_guid = GUID_INIT(0x822ace8f, 0x2814, 0x4174, 0xa5,
+					    0x6b, 0x5f, 0x02, 0x9f, 0xe0, 0x79, 0xee);
+
+struct cio2_gpio_regulator {
+	char regulator_name[GPIO_REGULATOR_NAME_LENGTH];
+	char supply_name[GPIO_REGULATOR_SUPPLY_NAME_LENGTH];
+	struct gpio_desc *gpio;
+	struct regulator_dev *rdev;
+	struct regulator_desc rdesc;
+	struct list_head list;
+};
+
+struct cio2_gpio_pin {
+	struct gpiod_lookup_table *lookup;
+	struct list_head list;
+};
+
+struct pmic {
+	struct device *dev;
+	struct acpi_device *adev;
+	unsigned int n_regulators;
+	struct list_head regulators;
+	unsigned int n_gpios;
+	struct list_head gpios;
+};
 
 struct sensor {
 	char name[ACPI_ID_LEN];
 	struct device *dev;
 	struct acpi_device *adev;
+	struct pmic pmic;
+	struct gpiod_lookup_table gpios;
 	struct software_node swnodes[6];
 	struct property_entry dev_properties[3];
 	struct property_entry ep_properties[4];
@@ -82,7 +143,7 @@ struct sensor_bios_data_packed {
 	u8 reserved1[3];
 	u8 mclkport;
 	u8 reserved2[13];
-} __packed__;
+};
 
 /* Fields needed by bridge driver */
 struct sensor_bios_data {
@@ -93,4 +154,42 @@ struct sensor_bios_data {
 	u32 mclkspeed;
 };
 
+struct pmic_cldb {
+	u8 version;
+	/*
+	 * control logic type
+	 * 0: UNKNOWN
+	 * 1: DISCRETE(CRD-D)
+	 * 2: PMIC TPS68470
+	 * 3: PMIC uP6641
+	 */
+	u8 control_logic_type;
+	u8 control_logic_id;
+	u8 sensor_card_sku;
+	u8 reserved[28];
+};
+
+struct cio2_sensor_regulator_map {
+	char *sensor_module_name;
+	unsigned int n_supplies;
+	struct regulator_consumer_supply *supplies;
+};
+
+/*
+ * Here follows platform specific mapping information that we can pass to
+ * regulator_init_data when we register our regulators. They're just mapped
+ * via index, I.E. the first regulator pin that the code finds for the
+ * i2c-OVTI2680:00 device is avdd, the second is dovdd and so on.
+ */
+
+const struct regulator_consumer_supply miix_510_ov2680[] = {
+	{ "i2c-OVTI2680:00", "avdd" },
+	{ "i2c-OVTI2680:00", "dovdd" },
+};
+
+const struct cio2_sensor_regulator_map cio2_sensor_regulator_maps[] = {
+	{ "GNDF140809R", 2, miix_510_ov2680 },
+};
+
 #endif
+
